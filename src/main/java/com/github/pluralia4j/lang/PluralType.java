e82d1a6619ad15a5012c53bb1ac5c07e6cac5490
Rikki Mongoose <alexander.a.teut@gmail.com>
2021-05-30 18:47:00 +0300

    * comment PluralType

--- src/main/java/com/github/pluralia4j/lang/PluralType.java
+++ src/main/java/com/github/pluralia4j/lang/PluralType.java
@@ -1,5 +1,8 @@
 package com.github.pluralia4j.lang;
 
+/**
+ * Type of pluralisation attached a word. Different langauges has different systems depending on grammar.
+ */
 public enum PluralType {
     ZERO,
     ONE,
