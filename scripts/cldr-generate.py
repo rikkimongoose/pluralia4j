e887c775d3ee72fc4368faf818ea4e19603d6356
Rikki Mongoose <alexander.a.teut@gmail.com>
2021-07-06 11:44:54 +0300

    + pre-release of cldr-generate.py

---
+++ scripts/cldr-generate.py
@@ -0,0 +1,23 @@
+from mako.template import Template
+import argparse, json
+
+parser = argparse.ArgumentParser()
+parser.add_argument('t', type=argparse.FileType('r'), help="File with Mako template")
+parser.add_argument('json', type=argparse.FileType('r'), help="File with JSON data")
+args = parser.parse_args()
+
+json_data = None
+with open(args.json) as file:
+	json_data = json.load(json_file)
+
+template_data = None
+with open(args.t) as file:
+	template_data = f.read()
+
+for lang, lang_data in json_data.items():
+	for code, code_data in lang_data:
+    	new_file_name = args.t.name.replace(".template", f".{code}")
+    	new_file_contents = Template(template_data).render(data=code_data, lang=lang)
+    	print(f"Creating {new_file_name}")
+    	with open(new_file_name, "w") as file_output:
+    		file_output.write(new_file_contents)
