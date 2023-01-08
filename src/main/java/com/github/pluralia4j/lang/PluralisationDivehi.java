52588fba1dd2b550dcd5a2685df1396e78c52a94
Rikki Mongoose <alexander.a.teut@gmail.com>
2023-01-08 17:21:02 +0300

    + add support for the following languages: Bulgarian, Colognian, Divehi

---
+++ src/main/java/com/github/pluralia4j/lang/PluralisationDivehi.java
@@ -0,0 +1,9 @@
+package com.github.pluralia4j.lang;
+
+import com.github.pluralia4j.lang.templates.PluralisationOneOther;
+
+/**
+ * Basic pluralisation rules for Divehi language
+ */
+public final class PluralisationDivehi extends PluralisationOneOther {
+}
